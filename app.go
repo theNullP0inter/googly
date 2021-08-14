e4077fba0d0fd26ad97b751b8fea076a4bb1963e
theNullP0inter <nikhilallamsetti@gmail.com>
2021-08-14 15:11:27 +0530

    added some docs

--- app.go
+++ app.go
@@ -4,6 +4,9 @@ import (
 	"github.com/sarulabs/di/v2"
 )
 
+// AppInterface should be implemented by sub-apps
+//
+// Build(*di.Builder) is where you should inject dependencies for the sub-app
 type AppInterface interface {
-	Build(builder *di.Builder)
+	Build(*di.Builder)
 }
--- googly.go
+++ googly.go
@@ -11,28 +11,68 @@ import (
 	"github.com/theNullP0inter/googly/ingress"
 )
 
+// GooglyInterface Connects googly to your application.
+//
+// Dependencies should be injected using Inject() Method
+// Ingress can be registered using GetIngressPoints()
 type GooglyInterface interface {
 	Inject(builder *di.Builder)
 	GetIngressPoints(di.Container) []ingress.IngressInterface
 }
 
+// Googly manages your application through dependendency injection
+// and runs it using Ingress
+//
+// GooglyInterface can be used to register Global dependencies
+// InstalledApps maintains a list of all your sub apps.
 type Googly struct {
 	GooglyInterface
 	InstalledApps []AppInterface
 }
 
+// Build builds the application and sub-apps
+//
+// This is invoked by Run
+func (g *Googly) Build() *di.Builder {
+	builder, _ := di.NewBuilder()
+	g.Inject(builder)
+
+	for _, app := range g.InstalledApps {
+		app.Build(builder)
+	}
+	return builder
+}
+
+// RegisterIngressPoints will register all the ingress points returned in GetIngressPoints to GooglyCmd
+//
+// This is invoked by Run
 func (g *Googly) RegisterIngressPoints(root_cmd *cobra.Command, cnt di.Container) {
 	for _, ig := range g.GetIngressPoints(cnt) {
 		root_cmd.AddCommand(ig.GetEntryCommand())
 	}
 }
 
+// Run runs your application in the following order.
+//
+// Reads env
+// Injects all dependencties
+// Builds the sub-apps
+// Registers ingress points
+// Runs root command
 func Run(g *Googly) {
 
+	// setup env
 	viper.AutomaticEnv()
-	cnt := InitContainer(g)
+
+	// Build sub-apps
+	builder := g.Build()
+	container := builder.Build()
+
+	// Register Ingress
 	root_cmd := command.GooglyCmd
-	g.RegisterIngressPoints(root_cmd, cnt)
+	g.RegisterIngressPoints(root_cmd, container)
+
+	// Run
 	if err := root_cmd.Execute(); err != nil {
 		fmt.Println(err)
 		os.Exit(1)
