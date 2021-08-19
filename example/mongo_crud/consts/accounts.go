f1827145f21a5420157e6eb00f35d566c698d8d3
theNullP0inter <nikhilallamsetti@gmail.com>
2021-08-19 19:22:27 +0530

    removed contrib and examples

--- example/mongo_crud/consts/accounts.go
+++
@@ -1,5 +0,0 @@
-package consts
-
-const AccountsServiceName = "accounts_service"
-const AccountsResourceManagerName = "accounts_resource_management"
-const AccountsControllerName = "accounts_controller"
--- example/rdb_crud/accounts/model.go
+++
@@ -1,8 +0,0 @@
-package accounts
-
-import "github.com/theNullP0inter/googly/contrib/rdb"
-
-type Account struct {
-	rdb.RdbSoftDeleteBaseModel
-	Username string `gorm:"unique" json:"username"`
-}
