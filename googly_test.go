cc56d9d8a78bdbf0b81e287cc10f2c5a6f4aae14
theNullP0inter <nikhilallamsetti@gmail.com>
2021-08-21 19:20:50 +0530

    refactored mocks

--- googly_test.go
+++ googly_test.go
@@ -3,53 +3,9 @@ package googly
 import (
 	"testing"
 
-	"github.com/sarulabs/di/v2"
 	"github.com/spf13/cobra"
 	"github.com/stretchr/testify/mock"
-	"github.com/theNullP0inter/googly/ingress"
 )
-
-// MockApp is a mock for App
-type MockApp struct {
-	mock.Mock
-}
-
-func (a *MockApp) Build(builder *di.Builder) {
-	a.Called()
-}
-
-// MockGoogly is a mock for Googly
-type MockGoogly struct {
-	mock.Mock
-	GooglyRunner
-	InstalledApps []App
-}
-
-func (g *MockGoogly) Build() *di.Builder {
-	g.Called()
-	builder, _ := di.NewBuilder()
-
-	return builder
-
-}
-
-func (g *MockGoogly) RegisterIngressPoints(rootCmd *cobra.Command, cnt di.Container) {
-	g.Called()
-}
-
-// MockGooglyRunner is a mock  implementation for GooglyRunner
-type MockGooglyRunner struct {
-	mock.Mock
-}
-
-func (r *MockGooglyRunner) Inject(builder *di.Builder) {
-	r.Called()
-}
-
-func (r *MockGooglyRunner) GetIngressPoints(di.Container) []ingress.Ingress {
-	r.Called()
-	return []ingress.Ingress{}
-}
 
 // TestRun tests the Run() function
 func TestRun(t *testing.T) {
