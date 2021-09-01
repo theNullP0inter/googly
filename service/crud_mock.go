06f6fed696e03c02ceaf9d66a72ef1c534fed21b
theNullP0inter <nikhilallamsetti@gmail.com>
2021-09-01 22:13:21 +0530

    added mock crud service

---
+++ service/crud_mock.go
@@ -0,0 +1,35 @@
+package service
+
+import (
+	"github.com/stretchr/testify/mock"
+	"github.com/theNullP0inter/googly/resource"
+)
+
+type MockCrudService struct {
+	mock.Mock
+}
+
+func (s *MockCrudService) GetItem(id DataInterface) (DataInterface, *ServiceError) {
+	s.Called()
+	return new(resource.MockResource), nil
+}
+
+func (s *MockCrudService) GetList(req DataInterface) (DataInterface, *ServiceError) {
+	s.Called()
+	return []*resource.MockResource{new(resource.MockResource)}, nil
+}
+
+func (s *MockCrudService) Create(req DataInterface) (DataInterface, *ServiceError) {
+	s.Called()
+	return new(resource.MockResource), nil
+}
+
+func (s *MockCrudService) Update(id DataInterface, update DataInterface) *ServiceError {
+	s.Called()
+	return nil
+}
+
+func (s *MockCrudService) Delete(id DataInterface, update DataInterface) *ServiceError {
+	s.Called()
+	return nil
+}
